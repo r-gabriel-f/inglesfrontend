78bf0d70eff4831869dad89af0206a4d315e9dc5
robert <robert.flores@shipedge.com>
2024-12-03 15:11:05 -0400

    cambio tiempo vervales

--- src/data/VerbsTense.ts
+++ src/data/VerbsTense.ts
@@ -13,451 +13,278 @@ export interface VerbsTenseModel {
 export const verbsTense: VerbsTenseModel[] = [
   {
     id: 1,
-    title: 'Simple Present (To Be)',
+    title: 'Simple Present',
     ingles: '-',
     espanol: '-',
     tense: [
       {
         verb: 'Affirmative',
-        ingles: 'I am a student.',
-        espanol: 'Soy un estudiante.',
+        ingles: 'I eat apples.',
+        espanol: 'Yo como manzanas.',
       },
       {
         verb: 'Negative',
-        ingles: 'I am not a student.',
-        espanol: 'No soy un estudiante.',
+        ingles: 'I do not eat apples.',
+        espanol: 'Yo no como manzanas.',
       },
       {
         verb: 'Question',
-        ingles: 'Am I a student?',
-        espanol: '¿Soy un estudiante?',
+        ingles: 'Do I eat apples?',
+        espanol: '¿Como manzanas?',
       },
     ],
   },
   {
     id: 2,
-    title: 'Simple Past (To Be)',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'I was a student.',
-        espanol: 'Fui un estudiante.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'I was not a student.',
-        espanol: 'No fui un estudiante.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Was I a student?',
-        espanol: '¿Fui un estudiante?',
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: 'Simple Future (To Be)',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'I will be a student.',
-        espanol: 'Seré un estudiante.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'I will not be a student.',
-        espanol: 'No seré un estudiante.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Will I be a student?',
-        espanol: '¿Seré un estudiante?',
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: 'Simple Present (To Have)',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'I have a book.',
-        espanol: 'Tengo un libro.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'I do not have a book.',
-        espanol: 'No tengo un libro.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Do I have a book?',
-        espanol: '¿Tengo un libro?',
-      },
-    ],
-  },
-  {
-    id: 5,
-    title: 'Simple Past (To Have)',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'I had a book.',
-        espanol: 'Tuve un libro.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'I did not have a book.',
-        espanol: 'No tuve un libro.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Did I have a book?',
-        espanol: '¿Tuve un libro?',
-      },
-    ],
-  },
-  {
-    id: 6,
-    title: 'Simple Future (To Have)',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'I will have a book.',
-        espanol: 'Tendré un libro.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'I will not have a book.',
-        espanol: 'No tendré un libro.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Will I have a book?',
-        espanol: '¿Tendré un libro?',
-      },
-    ],
-  },
-  {
-    id: 7,
     title: 'Present Continuous',
     ingles: '-',
     espanol: '-',
     tense: [
       {
         verb: 'Affirmative',
-        ingles: 'I am eating dinner.',
-        espanol: 'Estoy cenando.',
+        ingles: 'I am eating an apple.',
+        espanol: 'Estoy comiendo una manzana.',
       },
       {
         verb: 'Negative',
-        ingles: 'I am not eating dinner.',
-        espanol: 'No estoy cenando.',
+        ingles: 'I am not eating an apple.',
+        espanol: 'No estoy comiendo una manzana.',
       },
       {
         verb: 'Question',
-        ingles: 'Am I eating dinner?',
-        espanol: '¿Estoy cenando?',
+        ingles: 'Am I eating an apple?',
+        espanol: '¿Estoy comiendo una manzana?',
       },
     ],
   },
   {
-    id: 8,
-    title: 'Past Continuous',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'I was eating dinner.',
-        espanol: 'Estaba cenando.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'I was not eating dinner.',
-        espanol: 'No estaba cenando.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Was I eating dinner?',
-        espanol: '¿Estaba cenando?',
-      },
-    ],
-  },
-  {
-    id: 9,
-    title: 'Future Continuous',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'I will be eating dinner.',
-        espanol: 'Estaré cenando.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'I will not be eating dinner.',
-        espanol: 'No estaré cenando.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Will I be eating dinner?',
-        espanol: '¿Estaré cenando?',
-      },
-    ],
-  },
-  {
-    id: 10,
+    id: 3,
     title: 'Present Perfect',
     ingles: '-',
     espanol: '-',
     tense: [
       {
         verb: 'Affirmative',
-        ingles: 'I have finished my homework.',
-        espanol: 'He terminado mi tarea.',
+        ingles: 'I have eaten an apple.',
+        espanol: 'He comido una manzana.',
       },
       {
         verb: 'Negative',
-        ingles: 'I have not finished my homework.',
-        espanol: 'No he terminado mi tarea.',
+        ingles: 'I have not eaten an apple.',
+        espanol: 'No he comido una manzana.',
       },
       {
         verb: 'Question',
-        ingles: 'Have I finished my homework?',
-        espanol: '¿He terminado mi tarea?',
-      },
-    ],
-  },
-  {
-    id: 11,
-    title: 'Past Perfect',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'I had finished my homework.',
-        espanol: 'Había terminado mi tarea.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'I had not finished my homework.',
-        espanol: 'No había terminado mi tarea.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Had I finished my homework?',
-        espanol: '¿Había terminado mi tarea?',
-      },
-    ],
-  },
-  {
-    id: 12,
-    title: 'Future Perfect',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'I will have finished my homework.',
-        espanol: 'Habré terminado mi tarea.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'I will not have finished my homework.',
-        espanol: 'No habré terminado mi tarea.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Will I have finished my homework?',
-        espanol: '¿Habré terminado mi tarea?',
+        ingles: 'Have I eaten an apple?',
+        espanol: '¿He comido una manzana?',
       },
     ],
   },
 
   {
-    id: 13,
+    id: 4,
     title: 'Present Perfect Continuous',
     ingles: '-',
     espanol: '-',
     tense: [
       {
         verb: 'Affirmative',
-        ingles: 'I have been studying.',
-        espanol: 'He estado estudiando.',
+        ingles: 'I have been eating apples.',
+        espanol: 'He estado comiendo manzanas.',
       },
       {
         verb: 'Negative',
-        ingles: 'I have not been studying.',
-        espanol: 'No he estado estudiando.',
+        ingles: 'I have not been eating apples.',
+        espanol: 'No he estado comiendo manzanas.',
       },
       {
         verb: 'Question',
-        ingles: 'Have I been studying?',
-        espanol: '¿He estado estudiando?',
+        ingles: 'Have I been eating apples?',
+        espanol: '¿He estado comiendo manzanas?',
       },
     ],
   },
   {
-    id: 14,
+    id: 5,
+    title: 'Past Simple',
+    ingles: '-',
+    espanol: '-',
+    tense: [
+      {
+        verb: 'Affirmative',
+        ingles: 'I ate an apple.',
+        espanol: 'Comí una manzana.',
+      },
+      {
+        verb: 'Negative',
+        ingles: 'I did not eat an apple.',
+        espanol: 'No comí una manzana.',
+      },
+      {
+        verb: 'Question',
+        ingles: 'Did I eat an apple?',
+        espanol: '¿Comí una manzana?',
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: 'Past Continuous',
+    ingles: '-',
+    espanol: '-',
+    tense: [
+      {
+        verb: 'Affirmative',
+        ingles: 'I was eating an apple.',
+        espanol: 'Estaba comiendo una manzana.',
+      },
+      {
+        verb: 'Negative',
+        ingles: 'I was not eating an apple.',
+        espanol: 'No estaba comiendo una manzana.',
+      },
+      {
+        verb: 'Question',
+        ingles: 'Was I eating an apple?',
+        espanol: '¿Estaba comiendo una manzana?',
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: 'Past Perfect',
+    ingles: '-',
+    espanol: '-',
+    tense: [
+      {
+        verb: 'Affirmative',
+        ingles: 'I had eaten an apple.',
+        espanol: 'Había comido una manzana.',
+      },
+      {
+        verb: 'Negative',
+        ingles: 'I had not eaten an apple.',
+        espanol: 'No había comido una manzana.',
+      },
+      {
+        verb: 'Question',
+        ingles: 'Had I eaten an apple?',
+        espanol: '¿Había comido una manzana?',
+      },
+    ],
+  },
+  {
+    id: 8,
     title: 'Past Perfect Continuous',
     ingles: '-',
     espanol: '-',
     tense: [
       {
         verb: 'Affirmative',
-        ingles: 'I had been studying.',
-        espanol: 'Había estado estudiando.',
+        ingles: 'I had been eating apples.',
+        espanol: 'Había estado comiendo manzanas.',
       },
       {
         verb: 'Negative',
-        ingles: 'I had not been studying.',
-        espanol: 'No había estado estudiando.',
+        ingles: 'I had not been eating apples.',
+        espanol: 'No había estado comiendo manzanas.',
       },
       {
         verb: 'Question',
-        ingles: 'Had I been studying?',
-        espanol: '¿Había estado estudiando?',
+        ingles: 'Had I been eating apples?',
+        espanol: '¿Había estado comiendo manzanas?',
       },
     ],
   },
   {
-    id: 15,
+    id: 9,
+    title: 'Future Simple',
+    ingles: '-',
+    espanol: '-',
+    tense: [
+      {
+        verb: 'Affirmative',
+        ingles: 'I will eat an apple.',
+        espanol: 'Comeré una manzana.',
+      },
+      {
+        verb: 'Negative',
+        ingles: 'I will not eat an apple.',
+        espanol: 'No comeré una manzana.',
+      },
+      {
+        verb: 'Question',
+        ingles: 'Will I eat an apple?',
+        espanol: '¿Comeré una manzana?',
+      },
+    ],
+  },
+  {
+    id: 10,
+    title: 'Future Continuous',
+    ingles: '-',
+    espanol: '-',
+    tense: [
+      {
+        verb: 'Affirmative',
+        ingles: 'I will be eating an apple.',
+        espanol: 'Estaré comiendo una manzana.',
+      },
+      {
+        verb: 'Negative',
+        ingles: 'I will not be eating an apple.',
+        espanol: 'No estaré comiendo una manzana.',
+      },
+      {
+        verb: 'Question',
+        ingles: 'Will I be eating an apple?',
+        espanol: '¿Estaré comiendo una manzana?',
+      },
+    ],
+  },
+  {
+    id: 11,
+    title: 'Future Perfect',
+    ingles: '-',
+    espanol: '-',
+    tense: [
+      {
+        verb: 'Affirmative',
+        ingles: 'I will have eaten an apple.',
+        espanol: 'Habré comido una manzana.',
+      },
+      {
+        verb: 'Negative',
+        ingles: 'I will not have eaten an apple.',
+        espanol: 'No habré comido una manzana.',
+      },
+      {
+        verb: 'Question',
+        ingles: 'Will I have eaten an apple?',
+        espanol: '¿Habré comido una manzana?',
+      },
+    ],
+  },
+  {
+    id: 12,
     title: 'Future Perfect Continuous',
     ingles: '-',
     espanol: '-',
     tense: [
       {
         verb: 'Affirmative',
-        ingles: 'I will have been studying.',
-        espanol: 'Habré estado estudiando.',
+        ingles: 'I will have been eating apples.',
+        espanol: 'Habré estado comiendo manzanas.',
       },
       {
         verb: 'Negative',
-        ingles: 'I will not have been studying.',
-        espanol: 'No habré estado estudiando.',
+        ingles: 'I will not have been eating apples.',
+        espanol: 'No habré estado comiendo manzanas.',
       },
       {
         verb: 'Question',
-        ingles: 'Will I have been studying?',
-        espanol: '¿Habré estado estudiando?',
-      },
-    ],
-  },
-
-  {
-    id: 16,
-    title: 'Present Simple Passive',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'The book is read by many.',
-        espanol: 'El libro es leído por muchos.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'The book is not read by many.',
-        espanol: 'El libro no es leído por muchos.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Is the book read by many?',
-        espanol: '¿Es el libro leído por muchos?',
-      },
-    ],
-  },
-  {
-    id: 17,
-    title: 'Past Simple Passive',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'The book was read by many.',
-        espanol: 'El libro fue leído por muchos.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'The book was not read by many.',
-        espanol: 'El libro no fue leído por muchos.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Was the book read by many?',
-        espanol: '¿Fue el libro leído por muchos?',
-      },
-    ],
-  },
-  {
-    id: 18,
-    title: 'Future Simple Passive',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'The book will be read by many.',
-        espanol: 'El libro será leído por muchos.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'The book will not be read by many.',
-        espanol: 'El libro no será leído por muchos.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Will the book be read by many?',
-        espanol: '¿Será el libro leído por muchos?',
-      },
-    ],
-  },
-  {
-    id: 19,
-    title: 'Present Conditional',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      { verb: 'Affirmative', ingles: 'I would eat.', espanol: 'Comería.' },
-      { verb: 'Negative', ingles: 'I would not eat.', espanol: 'No comería.' },
-      { verb: 'Question', ingles: 'Would I eat?', espanol: '¿Comería?' },
-    ],
-  },
-  {
-    id: 20,
-    title: 'Past Conditional',
-    ingles: '-',
-    espanol: '-',
-    tense: [
-      {
-        verb: 'Affirmative',
-        ingles: 'I would have eaten.',
-        espanol: 'Habría comido.',
-      },
-      {
-        verb: 'Negative',
-        ingles: 'I would not have eaten.',
-        espanol: 'No habría comido.',
-      },
-      {
-        verb: 'Question',
-        ingles: 'Would I have eaten?',
-        espanol: '¿Habría comido?',
+        ingles: 'Will I have been eating apples?',
+        espanol: '¿Habré estado comiendo manzanas?',
       },
     ],
   },
